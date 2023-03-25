7760e8dba46c94f50235d40b8d0b63d5f240a535
Tobias Ullrich <ullrich.tobias@gmx.de>
2023-03-25 21:50:58 +0100

    more

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,22 +10,53 @@ import { ServerRequestService } from '../services/server-request.service';
 export class HeaderComponent implements OnInit {
   
   token: any = '';
+  actroute: string = '';
 
-  constructor(private router: Router, private servReqService: ServerRequestService) { }
+  constructor(private router: Router, private servReqService: ServerRequestService) {
+      this.actroute = this.router.url;
+  }
 
   ngOnInit(): void {
+
   }
 
   logoutUser(){
+    this.showSpinner();
     console.log('POST-Request to Backend to logout'); 
     this.token = localStorage.getItem('token');
-   this.servReqService.logoutUser({'token': this.token}).subscribe((data: {}) => {
-     let response: any = data;
-     console.log('Delete the Token of the User! ', response);
-     localStorage.removeItem('token');
-     console.log('Go to Logout-Site');
-     this.router.navigateByUrl('/logout');  
-   });
 
+   this.servReqService.logoutUser({'token': this.token}).subscribe({
+    next: (data: {}) => {
+      console.log(data);
+      let response: any = data;
+      //console.log('Delete the Token of the User! ', response);
+      localStorage.removeItem('token');
+      //console.log('Go to Logout-Site');
+      this.router.navigateByUrl('/logout');
+    },
+    error: (error: any) => {
+      //console.log('Fehler beim Einloggen:', error);
+      //console.log('Fehler beim Einloggen:', error.status);
+      //this.showErrorMessage(error.statusText);
+    }
+  });
+  this.hideSpinner();
   }
+
+  goToLogin(){
+    this.router.navigateByUrl('/login'); 
+  }
+
+  showSpinner(){
+    (<HTMLInputElement>document.getElementById('btn')).classList.add('red-col');
+    (<HTMLInputElement>document.getElementById('spinner')).classList.remove('d-none');
+    (<HTMLInputElement>document.getElementById('spinner')).classList.add('d-block'); 
+  }
+
+  hideSpinner(){
+    (<HTMLInputElement>document.getElementById('btn')).classList.remove('red-col');
+    (<HTMLInputElement>document.getElementById('spinner')).classList.add('d-none');
+    (<HTMLInputElement>document.getElementById('spinner')).classList.remove('d-block'); 
+  }
+
 }
